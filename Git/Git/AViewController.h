76f2890ac2f9ce185246e6ca4f1e064085103928
sw <875930370@qq.com>
2015-11-11 15:45:41 +0800

    A commit change pro

---
+++ Git/Git/AViewController.h
@@ -0,0 +1,13 @@
+//
+//  AViewController.h
+//  Git
+//
+//  Created by qianfeng on 15/11/11.
+//  Copyright (c) 2015年 孙雯雯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AViewController : UIViewController
+
+@end
---
+++ Git/Git/AViewController.m
@@ -0,0 +1,37 @@
+//
+//  AViewController.m
+//  Git
+//
+//  Created by qianfeng on 15/11/11.
+//  Copyright (c) 2015年 孙雯雯. All rights reserved.
+//
+
+#import "AViewController.h"
+
+@interface AViewController ()
+
+@end
+
+@implementation AViewController
+//A
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
